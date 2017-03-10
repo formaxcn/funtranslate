7f48f5378d29a29d322d67b8a8f70146c904be75
Tang Zhihao <tjrtzh@gmail.com>
2017-03-10 17:11:57 +0800

    update demo

---
+++ translate.js
@@ -0,0 +1,30 @@
+var Translate = (function(){
+	refreshTrans = function(){
+		var source = $('#input_text').val();
+		var type = $('#target').val();
+		var ggurl="https://translate.google.com/#auto/"+type+"/"+source;
+		var bdurl="http://fanyi.baidu.com/#auto/"+type+"/"+source;
+		$('#ggt').attr('src', ggurl);
+		$('#bdt').attr('src', bdurl);
+	};
+	
+	bindEvent = function(){
+		$('#input_text').on('blur',function(){
+			refreshTrans();
+		});
+		
+		$('#target').on('change',function(){
+			refreshTrans();
+		});
+	};
+	
+	return {
+		init:function(){
+			bindEvent();
+		}
+	}
+})();
+
+jQuery(document).ready(function () {
+    Translate.init();
+});
